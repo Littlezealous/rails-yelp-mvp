6215ee669051544754712ea7757e08d1077dcdbe
Littlezealous <zulaikha92@gmail.com>
2023-02-15 21:01:00 +0800

    add restaurant and reviews

--- db/seeds.rb
+++ db/seeds.rb
@@ -5,3 +5,11 @@
 #
 #   movies = Movie.create([{ name: "Star Wars" }, { name: "Lord of the Rings" }])
 #   Character.create(name: "Luke", movie: movies.first)
+Restaurant.delete_all
+Restaurant.create(name: "Epicure", category: "french", address: "yishun", phone_number: "1234")
+Restaurant.create(name: "amiro", category: "chinese", address: "sengkang", phone_number: "1111")
+Restaurant.create(name: "yaloa", category: "belgian", address: "telok ayer", phone_number: "2323")
+Restaurant.create(name: "hola", category: "italian",address: "woodland", phone_number: "4343")
+Restaurant.create(name: "jojolez", category: "japanese", address: "jurong", phone_number: "9875")
+
+#new +save = create
